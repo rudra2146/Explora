2d563d2700cbff616a81966684ba807900963e1c
Rudra Pandya <viralpandya079@gmail.com>
2025-06-26 18:35:54 +0530

    Header Done

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles'
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+  const coordinates = { lat:0,lng:0}
+  return (
+    <div>
+      <div className={classes.mapContainer}>
+        <GoogleMapReact
+                    bootstrapURLKeys={{key:'AIzaSyBhOs7sh-zWBeE1E_2fdpCHtR9hoP-4EQY'}}
+                    defaultCenter={coordinates}
+                    center={coordinates}
+                    defaultZoom={14}
+                    margin={[50,50,50,50]}
+                    // options={''}
+                    // onChange={''}
+                    // onChildClick={''}
+                    >
+
+        </GoogleMapReact>
+      </div>      
+    </div>
+  )
+}
+
+export default Map
